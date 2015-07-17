38c819cb1a1e652365e700cbcb81ec476bb5df54
Gowrie Sammandhamoorthy <smgowry@Gowries-MacBook-Pro.local>
2015-07-17 16:53:23 -0700

    Initial Commit

---
+++ WeatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherApp
+//
+//  Created by Gowrie Sammandhamoorthy on 7/17/15.
+//  Copyright (c) 2015 Gowrie Sammandhamoorthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherApp
+//
+//  Created by Gowrie Sammandhamoorthy on 7/17/15.
+//  Copyright (c) 2015 Gowrie Sammandhamoorthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
