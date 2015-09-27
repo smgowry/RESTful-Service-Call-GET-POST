73155f20e7d145fedf36ef887bc53c940fd3c9e3
smgowry <sm_gowry@yahoo.com>
2015-09-27 09:09:53 -0700

    Edited

--- WeatherApp/ViewController.h
+++ WeatherApp/ViewController.h
@@ -11,5 +11,18 @@
 @interface ViewController : UIViewController
 
 
+- (IBAction)zipcodeText:(UITextField *)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *currentImage;
+@property (weak, nonatomic) IBOutlet UILabel *cityNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *temparatureLabel;
+@property (weak, nonatomic) IBOutlet UILabel *weatherConditionLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *precipitationLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *humidityLabel;
+@property (weak, nonatomic) IBOutlet UITextField *tempa;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activatorInt;
+
 @end
 
--- WeatherApp/ViewController.m
+++ WeatherApp/ViewController.m
@@ -9,15 +9,80 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+- (IBAction)zipcodeText:(UITextField *)sender;{
+    [_activatorInt startAnimating];
+    
+   // NSLog(@"%@",sender.text);
+NSString*urlstring=[NSString stringWithFormat:@"http://api.openweathermap.org/data/2.5/weather?zip=%@,us",sender.text];
+
+NSURL*url=[NSURL URLWithString:urlstring];
+    
+NSURLRequest*requesturl =[NSURLRequest requestWithURL:url];
+[NSURLConnection sendAsynchronousRequest:requesturl queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse*response, NSData*data, NSError*connectionError){
+        if(data){
+NSDictionary*respo=[NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+ 
+self.cityNameLabel.text=respo[@"name"];
+         
+//To get sub dictionary value
+    
+           NSString*temparatureString=respo[@"main"][@"temp"];
+//To get float value in label
+            self.temparatureLabel.text=[NSString stringWithFormat:@"%f",temparatureString.floatValue];
+            NSString*humidityString=respo[@"main"][@"humidity"];
+//To get long integer value in label
+            self.humidityLabel.text=[NSString stringWithFormat:@"%lo",humidityString.integerValue];
+            
+//Toget weather condition. data is an Array
+        
+            NSArray*weatherArray=respo[@"weather"][0][@"main"];
+            NSArray*weatherConditionArray=respo[@"weather"][0][@"description"];
+            self.weatherConditionLabel.text=[NSString stringWithFormat:@"%@",weatherConditionArray];
+            self.precipitationLabel.text=[NSString stringWithFormat:@"%@",weatherArray];
+ 
+           self.currentImage.image =[UIImage imageNamed:@"sunny.jpeg"]; 
+            
+            
+  /*          for
+                (self.weatherConditionLabel.text=@"sunny");
+                 {
+                self.currentImage.image =[UIImage imageNamed:@"rain.jpeg"];
+                 }
+    
+else if (respo[@"weather"][0])[@"description"]="sky is clear" ) {
+                self.currentImage.image =[UIImage imageNamed:@"sunny.jpeg"];
+            }
+            
+else   {
+    self.currentImage.image =[UIImage imageNamed:@"allweather.jpeg"];
+}
+   */
+            
+           [_activatorInt stopAnimating];
+            
+      
+        }
+    }];
+    
+}
+
+
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
